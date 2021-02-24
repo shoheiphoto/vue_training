f82dc69a16b4412de1f20d3b7d43239918142c0e
shohei <sh12x29@gmail.com>
2021-02-24 16:26:11 +0900

    first

---
+++ vue-todo-list/src/components/ToDoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <input v-model="inputValue">
+  <button v-on:click="handleClick">
+    ToDoを追加
+  </button>
+  <ul>
+    <li v-for="todo in todoItems" 
+    v-bind:key="todo.id"
+    v-on:click="todo.done = !todo.done">
+    <span v-if="todo.done">✓</span>
+      {{ todo.text }}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      inputValue: '',
+      todoItems: [
+        {id: 1, 
+        done: false,
+        text: 'Go out to sea'},
+        {id: 2, 
+        done: true,
+        text: 'Invite the first member'}
+      ]
+    }
+  },
+  methods: {
+    handleClick(){
+      this.todoItems.push({
+        id: this.todoItems.length + 1,
+        text: this.inputValue
+      })
+      // 入力をクリアする
+      this.inputValue = ''
+    }
+  }
+}
+</script>
